201d722aefeb65cc41cca5d0d318d03fbe7b563e
Talented Blockchain Developer <59509767+Talanted-Blockchain-Dev@users.noreply.github.com>
2022-03-30 21:04:31 +0800

    refferal page fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,193 @@
-import React from "react";
-import Header from "./Header";
+import React, { useEffect, useState } from "react";
 import Grid from "@material-ui/core/Grid";
 import img_bnb from "../assets/bnb.png";
 import img_bottom1 from "../assets/48711064.png";
 import img_bottom2 from "../assets/image.png";
+import { useWallet } from "use-wallet";
+import { ethers } from "ethers";
 
 export default function MainFlex() {
+  const [headerClass, setHeaderClass] = useState("");
+  const [headerClass1, setHeaderClass1] = useState("");
+  const viewMenu = () => {
+    setHeaderClass("header_responsive");
+    setHeaderClass1("");
+  };
+  const hideMenu = () => {
+    setHeaderClass1("header_responsive_hide");
+    setHeaderClass("");
+  };
+
+  /* ------------ connect wallet --------------*/
+
+  const wallet = useWallet();
+  var styledAddress = wallet.account
+    ? wallet.account.slice(0, 4) + "..." + wallet.account.slice(-4)
+    : "";
+
+  //check connection
+  const handleChainChanged = (chainId) => {
+    let { ethereum } = window;
+    if (ethereum.isConnected() && Number(chainId) === 4002) {
+      onConnect();
+    }
+  };
+
+  const onConnect = () => {
+    if (wallet.status !== "connected") {
+      wallet.connect().catch((err) => {
+        alert("please check metamask!");
+      });
+    }
+  };
+
+  const disconnect = () => {
+    if (wallet.status === "connected") {
+      wallet.reset();
+    }
+  };
+  useEffect(() => {
+    const checkConnection = async () => {
+      let { ethereum } = window;
+      if (ethereum !== undefined) {
+        const chainId = await ethereum.request({ method: "eth_chainId" });
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const accounts = await provider.listAccounts();
+        if (accounts.length !== 0 && Number(chainId) === 4002) {
+          onConnect();
+        }
+        ethereum.on("chainChanged", handleChainChanged);
+      }
+    };
+  }, []);
   return (
     <>
       <div className="container">
-        <Header  />
-        <Grid container style={{ marginTop: "100px"  }} justifyContent="center">
+        <div className="header" style={{}}>
+          <div className="header_logo">
+            <img src={img_bnb} className="header_logo_img" alt="logo" />
+            <div className="header_logo_text">
+              <h3>BNBStaker</h3>
+            </div>
+          </div>
+
+          <div className={"header_middle " + headerClass + headerClass1}>
+            <button onClick={hideMenu} className="close_btn">
+              X
+            </button>
+            <a
+              className="header_audit_btn"
+              href="https://hazecrypto.net/audit/BNBStaker-xyz"
+              target="new"
+            >
+              <h3>Audit</h3>
+            </a>
+            <a href="https://t.me/+fyj9hqPxLUFlMDIx" target="new">
+              <h3>Telegram</h3>
+            </a>
+            <a href="https://discord.gg/v6bQP4aqPP" target="new">
+              <h3>Discord</h3>
+            </a>
+            <a
+              href="https://medium.com/@BNBStakerClub/bnbstaker-xyz-earn-upto-20-daily-on-your-staked-bnb-token-d2769a1c7cb2"
+              target="new"
+            >
+              <h3>Documentation</h3>
+            </a>
+            <a
+              href="https://medium.com/@BNBStakerClub/bnbstaker-club-faq-6d6e2820d392"
+              target="new"
+            >
+              <h3>FAQ</h3>
+            </a>
+            <div className="header_audit_btn1">
+              {wallet.status === "connected" ? (
+                <button
+                  className="header_connect_btn"
+                  style={{
+                    textTransform: "none",
+                  }}
+                  onClick={disconnect}
+                >
+                  <h3 id="connect-label">{styledAddress}</h3>
+                </button>
+              ) : (
+                <button
+                  onClick={() => onConnect()}
+                  className="header_connect_btn"
+                  style={{
+                    textTransform: "none",
+                  }}
+                >
+                  {wallet.status === "connecting" ? (
+                    <div>
+                      <span
+                        className="spinner-border"
+                        role="status"
+                        style={{
+                          width: "1.5em",
+                          height: "1.5em",
+                          marginRight: 10,
+                        }}
+                      ></span>
+                      <span className="sr-only ">Loading...</span>
+                    </div>
+                  ) : (
+                    <div>
+                      <h3 id="connect-label">Connect</h3>
+                    </div>
+                  )}
+                </button>
+              )}
+            </div>
+          </div>
+          <div className="header_right">
+            {wallet.status === "connected" ? (
+              <button
+                className="header_connect_btn"
+                style={{
+                  textTransform: "none",
+                }}
+                onClick={disconnect}
+              >
+                <h3 id="connect-label">{styledAddress}</h3>
+              </button>
+            ) : (
+              <button
+                onClick={() => onConnect()}
+                className="header_connect_btn"
+                style={{
+                  textTransform: "none",
+                }}
+              >
+                {wallet.status === "connecting" ? (
+                  <div>
+                    <span
+                      className="spinner-border"
+                      role="status"
+                      style={{
+                        width: "1.5em",
+                        height: "1.5em",
+                        marginRight: 10,
+                      }}
+                    ></span>
+                    <span className="sr-only ">Loading...</span>
+                  </div>
+                ) : (
+                  <div>
+                    <h3 id="connect-label">Connect</h3>
+                  </div>
+                )}
+              </button>
+            )}
+          </div>
+          <button onClick={viewMenu} className="header_menu_btn">
+            <div></div>
+            <div></div>
+            <div></div>
+          </button>
+        </div>
+        <Grid container style={{ marginTop: "100px" }} justifyContent="center">
           <Grid item xs={12} md={6}>
             <div className="home_f_money">
               <a
@@ -114,8 +291,8 @@ export default function MainFlex() {
             </div>
           </Grid>
         </Grid>
-        <Grid container justifyContent="center" style={{ marginTop: "150px" }}>
-          <Grid item xs={12} md={4}>
+        <Grid container justifyContent="center" style={{ marginTop: "100px" }}>
+          <Grid item xs={12} md={5} lg={4}>
             <div className="home_t_grp">
               <div className="filler-header">
                 <h2>Plan 1</h2>
@@ -206,7 +383,7 @@ export default function MainFlex() {
               </div>
             </div>
           </Grid>
-          <Grid item xs={12} md={4}>
+          <Grid item xs={12} md={5} lg={4}>
             <div className="home_t_grp">
               <div className="filler-header">
                 <h2>Plan 2</h2>
@@ -297,7 +474,7 @@ export default function MainFlex() {
               </div>
             </div>
           </Grid>
-          <Grid item xs={12} md={4}>
+          <Grid item xs={12} md={5} lg={4}>
             <div className="home_t_grp">
               <div className="filler-header">
                 <h2>Plan 3</h2>
@@ -389,7 +566,148 @@ export default function MainFlex() {
             </div>
           </Grid>
         </Grid>
+        <Grid container justifyContent="ccenter" style={{ marginTop: "100px" }}>
+          <Grid item xs={12} md={5}>
+            <div className="home_s_grp">
+              <div className="glass1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  xlink="http://www.w3.org/1999/xlink"
+                  aria-hidden="true"
+                  role="img"
+                  className="iconify iconify--cryptocurrency"
+                  width="50"
+                  height="50"
+                  preserveAspectRatio="xMidYMid meet"
+                  viewBox="0 0 32 32"
+                  data-icon="cryptocurrency:bnb"
+                  data-width="50"
+                  data-height="50"
+                  style={{ color: "yellow" }}
+                >
+                  <path
+                    fill="currentColor"
+                    d="M16 32C7.163 32 0 24.837 0 16S7.163 0 16 0s16 7.163 16 16s-7.163 16-16 16zm-3.884-17.596L16 10.52l3.886 3.886l2.26-2.26L16 6l-6.144 6.144l2.26 2.26zM6 16l2.26 2.26L10.52 16l-2.26-2.26L6 16zm6.116 1.596l-2.263 2.257l.003.003L16 26l6.146-6.146v-.001l-2.26-2.26L16 21.48l-3.884-3.884zM21.48 16l2.26 2.26L26 16l-2.26-2.26L21.48 16zm-3.188-.002h.001L16 13.706L14.305 15.4l-.195.195l-.401.402l-.004.003l.004.003l2.29 2.291l2.294-2.293l.001-.001l-.002-.001z"
+                  ></path>
+                </svg>
+              </div>
+              <div className="home_s_grp_text1">
+                <h3>Total Staked</h3>
+              </div>
+              <h2>
+                <div className="loader">
+                  <div className="lds-ellipsis">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                  </div>
+                </div>
+              </h2>
+            </div>
 
+            <div className="home_s_grp">
+              <button className="withdraw_btn">Withdraw</button>
+              <div className="home_s_grp_text1">
+                <h3>Available to withdraw</h3>
+              </div>
+              <h2>
+                <div className="loader">
+                  <div className="lds-ellipsis">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                  </div>
+                </div>
+              </h2>
+            </div>
+          </Grid>
+          <Grid item xs={12} md={7}>
+            <div className="home_s_grp">
+              <div className="home_s_grp_text1">
+                <h3>Your Referral Link</h3>
+              </div>
+              <div className="horizontal-group">
+                <div className="ref-link">
+                  <input
+                    id="invite-link"
+                    disabled=""
+                    value="https://bnbstaker.xyz?ref=YOUR_ADDRESS"
+                  />
+                </div>
+                <div className="ref-copy">
+                  <svg
+                    className="MuiSvgIcon-root MuiSvgIcon-fontSizeSmall css-1k33q06"
+                    focusable="false"
+                    aria-hidden="true"
+                    viewBox="0 0 24 24"
+                    data-testid="ContentCopyIcon"
+                    onclick=""
+                  >
+                    <path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z"></path>
+                  </svg>
+                </div>
+              </div>
+              <Grid container justifyContent="center">
+                <Grid item xs={12} md={6}>
+                  <div className="home_s_grp_text1">
+                    <h3>Total Bonus Earned</h3>
+                    <div className="loader">
+                      <div className="lds-ellipsis">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                      </div>
+                    </div>
+                  </div>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                  <div className="home_s_grp_text1">
+                    <h3>Total Bonus Withdrawn</h3>
+                    <div className="loader">
+                      <div className="lds-ellipsis">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                      </div>
+                    </div>
+                  </div>
+                </Grid>
+              </Grid>
+              <Grid container justifyContent="center">
+                <Grid item xs={12} md={6}>
+                  <div className="home_s_grp_text1">
+                    <h3>Users Invited by You</h3>
+                    <div className="loader">
+                      <div className="lds-ellipsis">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                      </div>
+                    </div>
+                  </div>
+                </Grid>
+
+                <Grid item xs={12} md={6}>
+                  <div className="home_s_grp_text1">
+                    <h3>You will receive:</h3>
+                    <p>
+                      - 6% for each level 1 referral deposit
+                      <br />- 3% for each level 2 referral deposit
+                      <br />- 1% for each level 3 referral deposit
+                      <br />
+                      <br />
+                    </p>
+                  </div>
+                </Grid>
+              </Grid>
+            </div>
+          </Grid>
+        </Grid>
         <div className="bottom_imgae_grp">
           <div style={{ width: "190px" }}>
             <img src={img_bottom1} className="bottom_image" alt="bottom" />
